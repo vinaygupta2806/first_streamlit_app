5b80cebe2e6f95c39f1de83139c3ec01f0d1cfbf
Vinay Gupta <vguptag2806@gmail.com>
2023-10-29 23:20:51 +0530

    snowflake account added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import pandas
 import requests
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -36,3 +43,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # It show the normalized json in tabular form.
 streamlit.dataframe(fruityvice_normalized)
+
+
