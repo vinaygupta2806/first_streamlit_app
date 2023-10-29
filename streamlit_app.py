5c9dc7db3f714cc5f6c6d08e247ed5b29cdbe482
Vinay Gupta <vguptag2806@gmail.com>
2023-10-30 00:03:27 +0530

    Added button for insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,14 @@ if streamlit.button('Get Fruit Load List:'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#Allow the user to add or insert into list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
 
-streamlit.stop()
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+if streamlit.button('Add a fruit to List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_fun = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_fun)
