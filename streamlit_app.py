15e3d165f41882a80b47faf0ad5e01b42d976680
Vinay Gupta <vguptag2806@gmail.com>
2023-10-29 18:10:58 +0530

    normalize json and deleted json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# It normalize the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# It show the normalized json in tabular form.
 streamlit.dataframe(fruityvice_normalized)
